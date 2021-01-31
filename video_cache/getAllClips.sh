9dd86e6c2996f1ab2f9a7916df6c1843813e07e2
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-01-31 09:08:37 -0800

    Add video download scripts

---
+++ video_cache/getAllClips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+start=$SECONDS
+input=$1
+rm mylist.txt
+
+while IFS= read -r line
+do
+  echo $line | sed 's/ //g' | sed 's/$/-withCaption.mp4/' | sed 's/^/file /' >> mylist.txt
+  ./getWordClip.sh $line
+done < "$input"
+
+ffmpeg -f concat -safe 0 -i mylist.txt -c copy OUTPUT.mp4 -y
+
+duration=$(( SECONDS - start ))
+echo "This took "
+echo $duration
+echo "seconds to run."
